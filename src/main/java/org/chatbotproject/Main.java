633522204a807c4215e0382f6bf73a258ade586a
Samuel Vítek <samuel.vitek@email.cz>
2024-03-22 15:24:28 +0200

    setting up main class, where the project will run

--- src/main/java/org/chatbotproject/Main.java
+++ src/main/java/org/chatbotproject/Main.java
@@ -1,13 +1,23 @@
 package org.chatbotproject;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner sc = new Scanner(System.in);
+    private static final String firstText = "\nThis is a loop test, please enter a string: ";
+
     public static void main(String[] args) {
+        System.out.println(firstText);
 
-        System.out.println("Hello and welcome!");
+        while (true) {
+            String userText = sc.nextLine();
 
-        for (int i = 1; i <= 5; i++) {
+            if (userText.equals("exit")) {
+                System.out.println("You are going to exit the program now.");
+                break;
+            }
 
-            System.out.println("i = " + i);
+            System.out.println("You wrote: " + userText);
         }
     }
 }
